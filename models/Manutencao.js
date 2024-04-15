aa34d9b9aebbcf25c27f2a21dde712bd3dc664c2
silvio <silviocesar.silva2903@gmail.com>
2024-04-15 14:03:34 -0300

    Ajustando o model de manutencao

--- models/Manutencao.js
+++ models/Manutencao.js
@@ -4,7 +4,7 @@ class Manutencao extends Model {
 
   static init(sequelize) {
     super.init({
-      descricacao: { 
+      descricao: { 
         type: DataTypes.STRING, 
         validate: {
           notEmpty: { msg: "O Login do Manutencao deve ser preenchido!" },
